c12b328cacd8b7cf18450388ec98886680b38dd6
Kevinallen005 <129138627+Kevinallen005@users.noreply.github.com>
2023-03-29 10:51:00 +0530

    Add files via upload

---
+++ day 1/check.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,n,i;
+	printf("enter the number upto which even numbers to be printed:");
+	scanf("%d",&n);
+	for(i=1;i=n;i=2)
+	{
+		printf("%d",i);
+	}
+	return 0;
+}
